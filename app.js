2dba207fa710fa5f0c8782b102fe71b2e18356be
Vijay Padmakumar <iamvijaypaddu@gmail.com>
2021-08-10 20:24:18 +0100

    updated action buttons sizes

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express")
 const stringHash = require("string-hash")
 const PORT = process.env.PORT || 3000
-const string = require("string-hash")
 
 const app = express()
 
