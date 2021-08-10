270b005168083c7f4adf1baecdd4d6193cffcffd
Vijay Padmakumar <iamvijaypaddu@gmail.com>
2021-08-10 16:01:48 +0100

    can create notes now, need to work on open feature and delete

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express")
+const stringHash = require("string-hash")
 const PORT = process.env.PORT || 3000
+const string = require("string-hash")
 
 const app = express()
+
+let notes = {}
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }))
@@ -9,12 +13,31 @@ app.use(express.static("public/css"))
 app.use(express.static("public/js"))
 
 app.get("/", (req, res) => {
-    res.render('home')
+    res.render('home', { all_notes: notes })
 })
 
 
 app.get("/compose", (req, res) => {
     res.render('compose')
+})
+
+app.post("/", (req, res) => {
+    let title = req.body.note_title
+    let text = req.body.note_text
+
+    /* needs to be moved to a module */
+
+    let hash = stringHash(title + text)
+
+    if (title.length && text.length) {
+        notes[hash] = [title, text]
+    }
+
+    console.log(notes);
+
+    /* end */
+
+    res.redirect("/")
 })
 
 app.listen(PORT, () => {
